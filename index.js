50d82f88dd76148a6f378cb436c2e78e5f8e0cfd
John Chen <johnchen062094@gmail.com>
2020-04-25 17:35:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var = "I love you, Grandma."
+  
+  if var.toLowerCase() === string {
+    console.log("I can't hear you!")
+  }
+  else if var.toUpperCase() === string {
+    console.log("YES INDEED!")
+  }
+  else if var === string {
+    console.log("I love you, too.")
+  }
+}
